a9dd092fe2522357fe136dabf42678511826e9dc
reconmri <86409474+reconmri@users.noreply.github.com>
2022-01-19 12:03:26 +0100

    Delete Matlab/SAMER_Matlab_code directory

--- Matlab/SAMER_Matlab_code/helper/fftc.m
+++
@@ -1,8 +0,0 @@
-function data = fftc( data, num )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-    data = fftshift(fft(fftshift(data,num),[], num),num);
-
-end
-
